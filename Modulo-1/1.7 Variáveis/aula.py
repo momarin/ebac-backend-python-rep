6a7ce1af5234a386bc549d32b0ed238c2934bcbf
Marina de Moraes <mrndemoraes@gmail.com>
2026-09-10 05:14:28 -0300

    finalizando variaveis

--- Modulo-1/1.7 Variáveis/aula.py
+++ Modulo-1/1.7 Variáveis/aula.py
@@ -1,3 +1,15 @@
-variavel = "Isso é uma variável string"
+# Variáveis
+variavel_string = "Isso é uma variável string"
+variavel_int = 3
+variavel_float = 3.14
+variavel_bool = True
 
-print(variavel)
+# Dicionario
+eu = {"pessoa":"Marina", "idade":34}
+
+# Iniciando
+print(variavel_string)
+print(variavel_int)
+print(variavel_float)
+print(variavel_bool)
+print(eu)
