0af7202fbd8ff99520e0f6de4d43af2866cce2a2
Marina de Moraes <mrndemoraes@gmail.com>
2026-09-10 05:48:13 -0300

    finalizando

---
+++ Modulo-1/1.8 Operadores/retangulo.py
@@ -0,0 +1,8 @@
+def calc_area_retangulo(largura, altura):
+    return largura * altura
+
+largura = 5
+altura = 10
+area = calc_area_retangulo(largura, altura)
+
+print(f"A área do retangulo é: {area}")
