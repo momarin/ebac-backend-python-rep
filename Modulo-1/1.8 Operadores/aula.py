6c3c103d6c2845db6d4805387762c9be2e28fd21
Marina de Moraes <mrndemoraes@gmail.com>
2026-09-10 05:26:53 -0300

    operadores

---
+++ Modulo-1/1.8 Operadores/aula.py
@@ -0,0 +1,37 @@
+# Operadores
+
+# Soma
+a = 5
+b = 12
+c = a + b
+print("A soma de 5 e 12 é:", c)
+
+# Subtração
+d = 13
+e = 6
+f = d - e
+print("A subtração entre 13 e 6 é:", f)
+
+# Divisão
+g = 10
+h = 2
+i = g / h
+print("A divisão entre 10 e 2 é:", i)
+
+# Divisão Inteira
+j = 10 
+k = 2
+l = j // k
+print("A divisão inteira entre 10 e 2 é:", l)
+
+# Multiplicação
+m = 3
+n = 6
+o = m * 6
+print("A multiplicação de 3 e 6 é:", o)
+
+# Potencialização
+p = 5
+q = 3
+r = p ** q
+print("5 elevado ao cubo é:", r)
