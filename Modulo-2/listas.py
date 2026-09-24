b07328aec2d90050c939063e9e2927d1a6027c04
Marina de Moraes <mrndemoraes@gmail.com>
2026-09-24 18:07:25 -0300

    listas

---
+++ Modulo-2/listas.py
@@ -0,0 +1,20 @@
+# Listas
+
+flores = ["rosa", "margarida", "violeta"]
+animais = ["gato", "cachorro", "rato"]
+frutas = ["banana", "kiwi", "ameixa"]
+
+# Inicializando
+print("==== Lista original ====")
+print(flores)
+print(animais)
+print(frutas)
+
+flores.append("crisântemo")
+animais.append("cavalo")
+frutas.append("maçã")
+
+print("==== Lista com adições ====")
+print(flores)
+print(animais)
+print(frutas)
